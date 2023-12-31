d4fedad5d0b4ea9076f4a8d843024b07d2aa8159
Ethan Lew <elew@galois.com>
2023-12-30 18:59:03 -0800

    add python module __init__

---
+++ concave_hull/__init__.py
@@ -0,0 +1,15 @@
+"""
+Concave Hull
+
+The concave hull core is written in Rust for performance.
+"""
+from concave_hull.concave_hull import concave_hull_2d
+
+__author__ = "Ethan Lew"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Ethan Lew"]
+__license__ = "GPLv3"
+__version__ = "0.1.0"
+__maintainer__ = "Ethan Lew"
+__email__ = "elew@galois.com"
+__status__ = "Production"
